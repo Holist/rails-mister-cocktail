e9989a9cc280650428f15b1b9baf6a96a699af7d
Romain Neuville <neuvilleromain@gmail.com>
2017-08-10 17:29:46 +0200

    Finished function

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,10 +1,29 @@
 class DosesController < ApplicationController
   def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail)
   end
 
   def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
   end
 
   def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = @cocktail.doses.build(doses_params)
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      @ingredients = Ingredient.all
+      render :new
+    end
+  end
+
+private
+
+  def doses_params
+    params.require(:dose).permit(:description, :ingredient_id)
   end
 end
