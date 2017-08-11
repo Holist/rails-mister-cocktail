83382e35382de62f6f835046367ec5d32f223a9e
Romain Neuville <neuvilleromain@gmail.com>
2017-08-11 16:23:00 +0200

    Finished

--- app/helpers/cocktails_helper.rb
+++ app/helpers/cocktails_helper.rb
@@ -1,7 +1,7 @@
 module CocktailsHelper
 
   def image_link(cocktail)
-    url = cocktail.photo? ? cocktail.photo.path : 'http://www.bar-a-cocktail.com/img/visuel1.jpg'
+    url = cocktail.photo? ? cocktail.photo.path : ''
     cl_image_path url, width: 350, height: 200, crop: :fill
   end
 
